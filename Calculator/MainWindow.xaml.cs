3f5e44c3ef7e9dcd381d44025a599a26829a4938
Spaiger <rostislavei@mail.ru>
2022-05-05 10:32:31 +1000

    Добавлены функции нажатия кнопок интерфейса

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -24,5 +24,173 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private bool zap = false;
+        float fis, sec, thi, memory, sqrt, perc;
+        int cou;
+        private bool abs = true;
+        private bool zero = true;
+        private bool ecv = false;
+        private bool zn = false;
+        private bool cl;
+        private void Button_Click_1(object sender, RoutedEventArgs e) //Number 1
+        {
+            if (zero)
+            {
+                Console.Clear();
+            }
+            if (cl)
+            {
+                Console.Clear();
+            }
+            Console.Text += "1";
+            zero = false;
+            ecv = false;
+            cl = false;
+
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e) //Number 2
+        {
+            if (zero)
+            {
+                Console.Clear();
+            }
+            if (cl)
+            {
+                Console.Clear();
+            }
+            Console.Text += "2";
+            zero = false;
+            ecv = false;
+            cl = false;
+
+        }
+        private void Button_Click_3(object sender, RoutedEventArgs e) //Number 3
+        {
+            if (zero)
+            {
+                Console.Clear();
+            }
+            if (cl)
+            {
+                Console.Clear();
+            }
+            Console.Text += "3";
+            zero = false;
+            ecv = false;
+            cl = false;
+
+        }
+        private void Button_Click_4(object sender, RoutedEventArgs e) //Number 4
+        {
+            if (zero)
+            {
+                Console.Clear();
+            }
+            if (cl)
+            {
+                Console.Clear();
+            }
+            Console.Text += "4";
+            zero = false;
+            ecv = false;
+            cl = false;
+
+        }
+        private void Button_Click_5(object sender, RoutedEventArgs e) //Number 5
+        {
+            if (zero)
+            {
+                Console.Clear();
+            }
+            if (cl)
+            {
+                Console.Clear();
+            }
+            Console.Text += "5";
+            zero = false;
+            ecv = false;
+            cl = false;
+
+        }
+        private void Button_Click_6(object sender, RoutedEventArgs e) //Number 6
+        {
+            if (zero)
+            {
+                Console.Clear();
+            }
+            if (cl)
+            {
+                Console.Clear();
+            }
+            Console.Text += "6";
+            zero = false;
+            ecv = false;
+            cl = false;
+
+        }
+        private void Button_Click_7(object sender, RoutedEventArgs e) //Number 7
+        {
+            if (zero)
+            {
+                Console.Clear();
+            }
+            if (cl)
+            {
+                Console.Clear();
+            }
+            Console.Text += "7";
+            zero = false;
+            ecv = false;
+            cl = false;
+
+        }
+        private void Button_Click_8(object sender, RoutedEventArgs e) //Number 8
+        {
+            if (zero)
+            {
+                Console.Clear();
+            }
+            if (cl)
+            {
+                Console.Clear();
+            }
+            Console.Text += "8";
+            zero = false;
+            ecv = false;
+            cl = false;
+
+        }
+        private void Button_Click_9(object sender, RoutedEventArgs e) //Number 9
+        {
+            if (zero)
+            {
+                Console.Clear();
+            }
+            if (cl)
+            {
+                Console.Clear();
+            }
+            Console.Text += "9";
+            zero = false;
+            ecv = false;
+
+        }
+        private void Button_Click_0(object sender, RoutedEventArgs e) //Number 0
+        {
+            if (zero)
+            {
+                Console.Clear();
+            }
+            if (cl)
+            {
+                Console.Clear();
+            }
+            Console.Text += "0";
+            zero = false;
+            ecv = false;
+            cl = false;
+        }
+        
     }
 }
