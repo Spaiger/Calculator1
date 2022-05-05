8e2888781a61b6b0f66bc2296c2d65683b1e6509
Spaiger <rostislavei@mail.ru>
2022-05-05 11:06:01 +1000

    Добавлены базовые операции с числами и вывод ответа
    Калькулятор с базовыми операциями полностью готов к использованию

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -191,6 +191,163 @@ namespace Calculator
             ecv = false;
             cl = false;
         }
-        
+        private void Button_Click_zap(object sender, RoutedEventArgs e) //Запятая
+        {
+            if (zap)
+            {
+                return;
+            }
+            Console.Text += ",";
+            zap = true;
+        }
+        private void Button_Click_plus(object sender, RoutedEventArgs e) //Operation plus
+        {
+            fis = float.Parse(Console.Text);
+            cou = 1;
+            /*if (!tap_plus)
+            {
+                fis = float.Parse(Console.Text);
+                shows = fis;
+            }
+            if (tap_plus)
+            {
+                thi = float.Parse(Console.Text);
+                shows = thi;
+                operations(thi);
+            }*/
+            Console.Clear();
+            if (zn)                                         //Добавление в предыдущие шаги
+            {
+                ShowSteps.Text += "+" + fis.ToString();
+            }
+            else
+            {
+                ShowSteps.Text += fis.ToString() + "+";
+            }
+            abs = true;
+            zn = false;
+            zap = false;
+            cl = true;
+            /*tap_plus = true;
+            tap_minus = false;
+            tap_multiplie = false;
+            tap_delim = false;*/
+        }
+        private void Button_Click_minus(object sender, RoutedEventArgs e) //Operation minus
+        {
+            fis = float.Parse(Console.Text);
+            Console.Clear();
+            cou = 2;
+            if (zn)                                         //Добавление в предыдущие шаги
+            {
+                ShowSteps.Text += "-" + fis.ToString();
+            }
+            else
+            {
+                ShowSteps.Text += fis.ToString() + "-";
+            }
+            abs = true;
+            zn = false;
+            zap = false;
+            cl = true;
+            /*tap_plus = false;
+            tap_minus = true;
+            tap_multiplie = false;
+            tap_delim = false;*/
+        }
+        private void Button_Click_multiplie(object sender, RoutedEventArgs e) //Operation multiplie
+        {
+            fis = float.Parse(Console.Text);
+            Console.Clear();
+            cou = 3;
+            if (zn)                                         //Добавление в предыдущие шаги
+            {
+                ShowSteps.Text += "*" + fis.ToString();
+            }
+            else
+            {
+                ShowSteps.Text += fis.ToString() + "*";
+            }
+            abs = true;
+            zn = false;
+            zap = false;
+            cl = true; ;
+            /*tap_plus = false;
+            tap_minus = false;
+            tap_multiplie = true;
+            tap_delim = false;*/
+        }
+        private void Button_Click_delim(object sender, RoutedEventArgs e) //Operation delim
+        {
+            fis = float.Parse(Console.Text);
+            Console.Clear();
+            cou = 4;
+            if (zn)                                         //Добавление в предыдущие шаги
+            {
+                ShowSteps.Text += "/" + fis.ToString();
+            }
+            else
+            {
+                ShowSteps.Text += fis.ToString() + "/";
+            }
+            abs = true;
+            zap = false;
+            zn = false;
+            cl = true;
+            /*tap_plus = false;
+            tap_minus = false;
+            tap_multiplie = false;
+            tap_delim = true;*/
+        }
+        float c;
+        private void Button_Click_ecv(object sender, RoutedEventArgs e) //Равно
+        {
+            float t = float.Parse(Console.Text);
+            int i = 0;
+            if (!ecv)
+            {
+                c = t;
+                zero = true;
+                ecv = true;
+                if (i == 0)
+                {
+                    ShowSteps.Clear();
+                    operations(t);
+                }
+                i++;
+            }
+            if (ecv)
+            {
+                ShowSteps.Clear();
+                operations(c);
+            }
+            fis = float.Parse(Console.Text);
+            zn = false;
+            zap = false;
+        }
+        private void operations(float t) //Проведение вычисления
+        {
+            switch (cou)
+            {
+                case 1:
+                    sec = fis + t;
+                    Console.Text = sec.ToString();
+                    break;
+                case 2:
+                    sec = fis - t;
+                    Console.Text = sec.ToString();
+                    break;
+                case 3:
+                    sec = fis * t;
+                    Console.Text = sec.ToString();
+                    break;
+                case 4:
+                    sec = fis / t;
+                    Console.Text = sec.ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
